7d80724c92a316e14aab5df28ed1bd11564e49b6
Kadi <kadi.steam@gmail.com>
2023-08-01 00:27:55 +0100

    fixing bugs

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use(
 app.use(csrfProtection);
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.isAuthenticated = req.session.isLoggedIn || false;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
