68929b7129c56ce6589f2ec41d0f35d00ca3a921
Kadi <kadi.steam@gmail.com>
2023-08-01 00:43:57 +0100

    bug fix

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use(
 app.use(csrfProtection);
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn || false;
+  res.locals.isAuthenticated = req.session?.isLoggedIn || false;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
@@ -97,7 +97,7 @@ app.use((error, req, res, next) => {
   res.status(500).render("500", {
     pageTitle: "Error!",
     path: "/500",
-    isAuthenticated: req.session.isLoggedIn,
+    isAuthenticated: req.session.isLoggedIn || false,
   });
 });
 app.use("/500", errorController.get500Page);
